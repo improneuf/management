2a22cc1d1946e8a058d9371ab66b94142fbd27e9
Pravin Dahal <pravin.dahal@optimeering.com>
2024-02-14 00:51:28 +0100

    feat: add simple program to create screenshots

---
+++ src/html-to-png/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	// Create a context
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	// Capture screenshot of an entire webpage in JPEG format
+	var buf []byte
+	if err := chromedp.Run(ctx,
+		chromedp.EmulateViewport(1920, 1004),
+		chromedp.Navigate(`http://localhost:8000/output/2024-02-14%20-%20Impro%20Neuf%20Wednesday%20Show.html`),
+		chromedp.ActionFunc(func(ctx context.Context) error {
+			// Set the zoom level by scaling the CSS
+			return chromedp.Evaluate(`document.body.style.zoom = "2"`, nil).Do(ctx)
+		}),
+		chromedp.FullScreenshot(&buf, 100),
+	); err != nil {
+		log.Fatal(err)
+	}
+
+	// Save the screenshot to a file
+	if err := ioutil.WriteFile("screenshot.jpg", buf, 0644); err != nil {
+		log.Fatal(err)
+	}
+}
