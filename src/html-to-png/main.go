6608f87cbe176ecf03c37aa54d813ca0517fe3ff
Pravin Dahal <pravin.dahal@optimeering.com>
2024-11-12 23:47:11 +0100

    fix: Fix zoom in html-to-png tool

--- src/html-to-png/main.go
+++ src/html-to-png/main.go
@@ -18,11 +18,11 @@ func main() {
 	if err := chromedp.Run(ctx,
 		chromedp.EmulateViewport(1920, 1004),
 		chromedp.Navigate(`
-			file:///Users/pravindahal/impro-neuf-management/src/show-program/output/2024-08-26%20-%20Impro%20Neuf%20Wednesday%20Show.html
+			file:///Users/pravindahal/impro-neuf-management/src/show-program/output/2024-11-06%20-%20Impro%20Neuf%20Wednesday%20Show%20-%20meetup.html
 		`),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			// Set the zoom level by scaling the CSS
-			return chromedp.Evaluate(`document.body.style.zoom = "2"`, nil).Do(ctx)
+			return chromedp.Evaluate(`document.body.style.zoom = "1"`, nil).Do(ctx)
 		}),
 		chromedp.FullScreenshot(&buf, 100),
 	); err != nil {
