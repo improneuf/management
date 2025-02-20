37fb974474191555e7236a7fa2c9d5fd2d94bf9f
Pravin Dahal <pravin.dahal@optimeering.com>
2025-02-20 22:21:00 +0100

    Update html-to-png with easy-to-duplicate code

--- src/html-to-png/main.go
+++ src/html-to-png/main.go
@@ -16,15 +16,16 @@ func main() {
 	// Capture screenshot of an entire webpage in JPEG format
 	var buf []byte
 	if err := chromedp.Run(ctx,
-		chromedp.EmulateViewport(1920, 1004),
+	    chromedp.EmulateViewport(1920, 1005),
+		//chromedp.EmulateViewport(1920, 1080),
 		chromedp.Navigate(`
-			file:///Users/pravindahal/impro-neuf-management/src/show-program/output/2024-11-06%20-%20Impro%20Neuf%20Wednesday%20Show%20-%20meetup.html
+			file:///Users/pravindahal/impro-neuf-management/src/open-workshops/haley-brigi.html
 		`),
 		chromedp.ActionFunc(func(ctx context.Context) error {
 			// Set the zoom level by scaling the CSS
 			return chromedp.Evaluate(`document.body.style.zoom = "1"`, nil).Do(ctx)
 		}),
-		chromedp.FullScreenshot(&buf, 100),
+		chromedp.CaptureScreenshot(&buf),
 	); err != nil {
 		log.Fatal(err)
 	}
